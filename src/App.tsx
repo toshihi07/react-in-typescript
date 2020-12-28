b2e5e6a403a767a5bbd41ba3bb8dcf61c02fbdd6
Toshihiro Shinmachi <toshihi0717@gmail.com>
2020-12-28 15:42:02 +0900

    function-components-basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+
+//optiononalnalな引数にしないと、index.tsxxで値を渡さなければエラーになる。
 interface AppProps {
-  message: string;
-  name: string
+  message?: string;
 }
 
 //AppPropsを型引数に指定。こうすることで、引数の型指定を省ける。
-const App: React.FunctionComponent<AppProps> = ({message}) => {
+//型引数<AppProps>をしていしないと、AppProps内の型指定が機能しない。言い換えればアノテーションを掛けなければならない。
+const App: React.FunctionComponent = ({message}) => {
   //プロパティを取り出すjavascriptの分割代入。
   //何でも受け取れる状態ではtypescriptを導入する意味がない。
   
   return <div>{ message }</div>;
 };
+
+//外から値が来なかったときにはこの値が採用される。必ずAppProps内にメンバが定義されていなければならない。
+App.defaultProps = {
+  message: 123,
+  description: 'This is app component'
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -9,4 +9,4 @@ type foo = JSX.IntrinsicAttributes;
 //index.htmlのid=rootの部分を<App />に置き換えますよと書いてある。
 //子コンポーネントであるappコンポーネントにmessageという文字列データを渡した。これによって、appコンポーネントの側からmessageを取り出せるようになった。
 //typescriptでは、データを渡す際に型を指定しないといけない。例外はIntrinsicAttributesでreactに指定されているkeyのみ。追加はできる。
-ReactDOM.render(<App message="hello react" name="ham"/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
