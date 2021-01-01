5a82dcec918ea6f7329608d06a35021491e64848
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-01 14:47:23 +0900

    useReducer-2

--- src/CounterUseReducer.tsx
+++ src/CounterUseReducer.tsx
@@ -1,13 +1,26 @@
 import React,{ useReducer } from 'react'
 
-const initialState = {count: 0};
 
-function reducer(state: any, action: any) {
+type ActionType = {
+  type: "decrement" | "increment" | "reset"
+}
+
+type StateType = {
+  count :number
+}
+
+const initialState: StateType = {count: 0};
+
+
+//状態を受け取って、新たな状態を返す関数。なので、戻り値はstateTypeか戻り値のないnever
+function reducer(state: StateType, action: ActionType): StateType | never{
   switch (action.type) {
     case 'increment':
       return {count: state.count + 1};
     case 'decrement':
       return {count: state.count - 1};
+    case 'reset':
+      return initialState
     default:
       throw new Error();
   }
@@ -21,6 +34,7 @@ function CounterUseReducer() {
       Count: {state.count}
       <button onClick={() => dispatch({type: 'decrement'})}>-</button>
       <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <button onClick={() => dispatch({type: 'reset'})}>*</button>
     </>
   );
 }
